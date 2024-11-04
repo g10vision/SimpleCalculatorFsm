08cc9ec233a16034bfc9bd631b53a1ca428a87ca
Gio Salazar <gxsalazar08@gmail.com>
2024-11-04 00:15:13 -0600

    update test cases

--- src/TestCalculator.java
+++ src/TestCalculator.java
@@ -1,10 +1,10 @@
 public class TestCalculator {
     public static void main(String[] args) {
+        CalculateTest("1+1-1");
+        CalculateTest("100+10+1");
+        CalculateTest("10A+1-10");
         CalculateTest("10.3-1+1");
         CalculateTest("1*-9");
-        //CalculateTest("10+1+1");
-        //CalculateTest("10+1+10");
-        //CalculateTest("10A+1-10");
     }
     public static void CalculateTest(String expression) {
         var evaluator = new Evaluator();
